ecf59d664019487af0c6bdc7575b49a8570aa04d
hlfs12345 <hlfs12345@hotmail.com>
2023-12-26 20:59:23 -0300

    fix name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from "@angular/router";
   template : `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-  title = 'Angular-Portifolio';
+  title = 'Angular-Portfolio';
 }
