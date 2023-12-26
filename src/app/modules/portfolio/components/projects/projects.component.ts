da4003c7870f4f9158bc4ac48475c55ba3d2dc03
Hilton <hilton.souza>
2023-12-26 16:39:27 -0300

    Dialog

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,7 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 
 //Interface
 import { IProjects } from '../../interface/IProjects.interface';
+
+//Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+//Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
+//Material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-projects',
@@ -11,6 +20,8 @@ import { IProjects } from '../../interface/IProjects.interface';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/DelphiProj.png',
@@ -70,5 +81,12 @@ export class ProjectsComponent {
     },
   
   ]);
+
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent, {
+      data, 
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 
 }
