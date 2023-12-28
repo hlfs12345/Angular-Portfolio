e3ca72524ac97a8d49907094874bdbe2183729e8
hlfs12345 <hlfs12345@hotmail.com>
2023-12-28 12:40:18 -0300

    knowledge

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -16,34 +16,52 @@ export class KnowledgeComponent {
     {
       src: 'assets/icons/knowledge/delphi.svg',
       alt: 'Ícone de conhecimento em Delphi',
+      title: 'Delphi',
     },
     {
       src: 'assets/icons/knowledge/aspnet.svg',
       alt: 'Ícone de conhecimento em Asp.Net Core',
+      title: 'Asp.Net Core',
     },
     {
       src: 'assets/icons/knowledge/csharp.svg',
       alt: 'Ícone de conhecimento em C Sharp',
+      title: 'C#',
     },
     {
       src: 'assets/icons/knowledge/angular.svg',
       alt: 'Ícone de conhecimento em Angular',
+      title: 'Angular',
     },
     {
       src: 'assets/icons/knowledge/typescript.svg',
       alt: 'Ícone de conhecimento em TypeScript',
+      title: 'TypeScript',
     },
     {
       src: 'assets/icons/knowledge/html.svg',
       alt: 'Ícone de conhecimento em HTML',
+      title: 'HTML',
     },
     {
       src: 'assets/icons/knowledge/css.svg',
       alt: 'Ícone de conhecimento em CSS',
+      title: 'CSS',
     },
     {
       src: 'assets/icons/knowledge/javascript.svg',
       alt: 'Ícone de conhecimento em JavaScript',
+      title: 'JavaScript',
+    },
+    {
+      src: 'assets/icons/knowledge/firebirdsql.svg',
+      alt: 'Ícone de conhecimento em Firebird SQL',
+      title: 'Firebird SQ',
+    },
+    {
+      src: 'assets/icons/knowledge/sqlserver.svg',
+      alt: 'Ícone de conhecimento em SQL Server',
+      title: 'SQL Server',
     },
   ]);
 
